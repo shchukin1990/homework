da0522884d327b8ceb03ae038d79abc58e3e1698
shhukin <nikitose398@mail.ru>
2022-02-10 20:46:27 +0300

    Поменял текст

--- homework/Program.cs
+++ homework/Program.cs
@@ -6,7 +6,7 @@ namespace homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Что-то!");
         }
     }
 }
