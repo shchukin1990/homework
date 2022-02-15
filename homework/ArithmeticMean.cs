4e15bfb2f9e62369c291c9e86043f9958a6eab06
shhukin <nikitose398@mail.ru>
2022-02-15 19:37:58 +0300

    1502

---
+++ homework/ArithmeticMean.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace homework
+{
+    public class ArithmeticMean
+    {
+
+        // S2.7. Даны два целых числа. Найти: а) их среднее арифметическое; б) их среднее геометрическое.
+        //public ArithmeticMean()
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа ");
+            double g = int.Parse(Console.ReadLine());
+            double t = int.Parse(Console.ReadLine());
+            double averageAr = (g + t) / 2;
+            double averageGeometry = Math.Sqrt(g * t);
+            Console.WriteLine(averageAr);
+            Console.WriteLine(averageGeometry);
+        }
+    }
+}
---
+++ homework/TightMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace homework
+{
+    public class TightMaterial
+    {
+        //static void Main(string[] args)
+       static void   density(string[]args)
+        {
+            //S2.8. Известны объем и масса тела. Определить плотность материала этого тела.
+
+            double value = long.Parse(Console.ReadLine());
+            double weight = long.Parse(Console.ReadLine());
+            double density = weight / value;//desity- плотность 
+            Console.WriteLine(density);
+
+        }
+    }
+}
