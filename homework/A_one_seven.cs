8157573ba3c57a5099f751a7abe6a4000d69f7ce
shhukin <nikitose398@mail.ru>
2022-02-19 17:23:10 +0300

    1902

---
+++ homework/A_one_seven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace homework
+{
+    public class A_one_seven
+    {
+       public A_one_seven()
+         //static void Main(string[] args)
+        {
+            //            A1.7.Дан прямоугольник с размерами 543 х 130 мм.Сколько квадратов со стороной 130 мм можно отрезать от него?
+            //Я сделал решение задачи с вводимыми координатами где одна из сторон прямоугольника равна стороне квадрата. Все ли верно ?
+
+            Console.WriteLine("Введите длину: ");
+            int lenth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ширину: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine(lenth / width);
+        }
+    }
+}
---
+++ homework/A_two_one.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace homework
+{
+    public class A_two_one
+    {
+        //public A_two_one()
+            static void Main(string[] args)
+        {
+            //A2.1.Дано двузначное число. Найти:
+            //а) число десятков в нем;
+            //б) число единиц в нем;
+            //в) сумму его цифр;
+            //г) произведение его цифр.
+            int dozens;
+            int units;
+            int sum;
+            Console.WriteLine("x=");
+            int x = int.Parse(Console.ReadLine());
+            dozens = x / 10;
+            units = x - (dozens * 10);
+            Console.WriteLine($"Количество десятков {dozens:f0}");
+            Console.WriteLine($"Количество единиц {units:f0}");
+            sum = dozens + units;
+            Console.WriteLine($"Сумма всех чисел двузначного числа {sum:f0}");
+        }
+    }
+}
