cb9354e8b438b3bbc58b15cc539ec6b00d572664
shhukin <nikitose398@mail.ru>
2022-02-20 19:42:19 +0300

    2002

---
+++ homework/AThreeFour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace homework
+{
+    public class AThreeFour
+    {
+        //public AThreeFour()
+       // A3.4. В трехзначном числе x зачеркнули первую цифру.
+       // Когда полученное число умножили на 10
+       // , а произведение сложили с первой цифрой числа x,
+       // то получилось число n.По заданному n найти число x
+       // (значение n вводится с клавиатуры, 100 ≤ n ≤ 999).
+            static void Main(string[] args)
+        {
+            int n;
+            
+            int buf;
+            int buf1;
+            int number;
+
+            Console.WriteLine("Введите число от 1 до 999");
+            n = int.Parse(Console.ReadLine());
+            while((n>999)|(n<1))
+            {
+                Console.WriteLine("Введите число от 1 до 999");
+                n = int.Parse(Console.ReadLine());
+            }
+            buf = n / 10;
+            buf1 = (n - buf * 10);
+            number = buf1 * 100 + buf;
+            Console.WriteLine("Число x = {0}", number);
+        }
+    }
+}
---
+++ homework/AThreeOne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace homework
+{
+    public class AThreeOne
+    {
+        public AThreeOne()
+        //A3.1 Из трехзначного числа x вычли его последнюю цифру.
+        //Когда результат разделили на 10, а к частному слева приписали
+        //последнюю цифру числа x, то получилось число 237. Найти число x.
+
+
+            //static void Main(string[] args)
+        {
+            int x, y, num, priv;
+            y = 237;
+            num = y / 100;
+            priv = y % 100;
+            x = priv * 10 + num;
+            Console.WriteLine(x);
+        }
+    }
+}
