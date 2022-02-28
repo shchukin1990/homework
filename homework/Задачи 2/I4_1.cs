9f82182096f7d950ca1bb842df21bebe60bd813d
shhukin <nikitose398@mail.ru>
2022-02-28 20:13:48 +0300

    2802

---
+++ homework/Задачи 2/I4_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Draft
+{
+    public class I4_1
+    {
+        //        I4.1. Даны два различных вещественных числа.Определить наибольшее из них, не используя полный условный оператор, а применив:
+        //а) два неполных условных оператора;
+        //        б) один неполный условный оператор.
+        public I4_1()
+            //static void Main(string[] args)
+        {
+            int n1 = 1;
+             int n2 = 2;
+            if (n1 > n2)
+                Console.WriteLine(n1 + " больше");
+            else
+                Console.WriteLine(n2 + " больше");
+        }
+    }
+}
---
+++ homework/Задачи 2/W_1_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Draft
+{
+    public class W_1_1
+    {// не понимаю
+     //        W1.1. Дана непустая последовательность целых чисел, оканчивающаяся нулем.Найти:
+     //а) сумму всех чисел последовательности;
+     //        б) количество всех чисел последовательности.
+        public W_1_1()
+            //static void Main(string[] args)
+        {
+            int n = 10;
+            int y = 90;
+            int f = 40;
+            while(f!=0)
+            {
+                Console.WriteLine("Введите число: ");
+               y = Convert.ToInt32(Console.ReadLine());
+                n += f;
+                n++;
+            }
+
+            Console.WriteLine("Сумма = {0}, всего чисел = {1}", y, n);
+            Console.ReadKey();
+        }
+    }
+}
